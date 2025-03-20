e01e23a29df5adc1de9fb96e06406d397bde09a2
Amir ali <amiralikhamseh01@gmail.com>
2025-03-20 18:06:38 +0100

    feature : features section added

---
+++ client/src/app/(nondashboard)/landing/const.ts
@@ -0,0 +1,14 @@
+export const titleArray = [
+  "Trustworthy and Verified Listings",
+  "Browse Rental Listings with Ease",
+  "Simplify Your Rental Search with Advanced",
+];
+export const descriptionArray = [
+  "Discover the best rental options with user reviews and ratings.",
+  "Get access to user reviews and ratings for a better understanding of rental options.",
+  "Find trustworthy and verified rental listings to ensure a hassle-free experience.",
+];
+export const linkTextArray = ["Explore", "Search", "Discover"];
+export const linkHrefArray = ["/explore", "/search", "/discover"];
+export const titleText =
+  " Quickly find the home you want using our effective search filters!";
