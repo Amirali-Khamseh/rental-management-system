4bfc45bf466f3923f3a3ff80d212b59c6c33c523
Amir ali <amiralikhamseh01@gmail.com>
2025-03-24 23:02:07 +0100

    feature : Auth BE implementation completed as well as tenat route and controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { authMiddleware } from "./middleware/authMiddleware";
+import tenantRoutes from "./routes/tenantRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,6 +22,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3002;
