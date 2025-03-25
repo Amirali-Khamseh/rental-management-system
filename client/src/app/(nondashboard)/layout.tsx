5b46b9125e33acb8ee095f87cedb6fad72243f49
Amir ali <amiralikhamseh01@gmail.com>
2025-03-25 21:18:58 +0100

    feature: RTK QUERY APIs completed based on BE , and dashboard releated pages and componenets added

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { NAVBAR_HEIGHT } from "../lib/constants";
+"use client";
+
 import Navbar from "@/components/navbar";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { NAVBAR_HEIGHT } from "../lib/constants";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
+
   return (
     <div className="h-full w-full">
       <Navbar />
@@ -16,4 +42,4 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default layout;
+export default Layout;
